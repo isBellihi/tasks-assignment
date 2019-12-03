dfafe19a1162692098e7a8a96903cff48a8e760a
isBellihi <ismail.bellihi@um5s.net.ma>
2019-12-03 14:55:35 +0100

    add gitignore and CI CD process

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/path', (req, res) => {
+    res.send('Hello Word 2!');
 });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
-
-//Run app, then load http://localhost:port in a browser to see the output.
